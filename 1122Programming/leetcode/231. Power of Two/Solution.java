bf93b9539876735566160cf86dfaa9ac5ca40a3d
Ateto1202 <90423964+Ateto1204@users.noreply.github.com>
2024-03-05 15:15:15 +0800

    Add files via upload

---
+++ 1122Programming/leetcode/231. Power of Two/Solution.java
@@ -0,0 +1,19 @@
+/*
+ *
+ * https://leetcode.com/problems/power-of-two/
+ * Problem ID: 231. Power of Two
+ * 
+ * Author: Ateto
+ * 
+ */
+
+class Solution {
+    public boolean isPowerOfTwo(int n) {
+        if(n <= 0) return false;
+        while(n != 1) {
+            if((n & 1) == 1) return false;
+            n >>= 1;
+        }
+        return true;
+    }
+}
