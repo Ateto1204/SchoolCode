9d16dbd3d6f267cc5cfa01219822f584a424f83c
Ateto1202 <tonysu1204@gmail.com>
2024-03-13 12:25:10 +0800

    Upload c source code

---
+++ 1122Programming/leetcode/2485. Find the Pivot Integer/ans.c
@@ -0,0 +1,21 @@
+/**
+ * https://leetcode.com/problems/find-the-pivot-integer/
+ * Problem ID: 2485. Find the Pivot Integer
+ *
+ * Author: Ateto
+ */
+
+int cal(int p, int q) {
+    int tmp = (p + q) * (q - p + 1) / 2;
+    return tmp;
+}
+
+int pivotInteger(int n) {
+    for(int i = n; i >= 0; --i) {
+        int x = cal(1, i);
+        int y = cal(i, n);
+        if(x == y) return i;
+        if(x < y) break;
+    }
+    return -1;
+}
